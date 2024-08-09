39a2a6de9070bbce4f322b942dfbc34a0f3dfb28
ali-lutfalla <ali,latfalla@gmail.com>
2024-08-10 02:33:19 +0300

    Finished the functions

---
+++ src/components/BurgerStack.jsx
@@ -0,0 +1,13 @@
+// `src/components/BurgerStack.jsx`
+const BurgerStack = (props) => {
+    return <ul>
+        {props.stack.map((ingredient, index) => (
+            <li key={index} style={{ backgroundColor: ingredient.color }}>
+              {ingredient.name}
+              <button onClick={() => props.removeFromBurger(index)}>-</button>
+            </li>
+        ))}
+    </ul>;
+  };
+  
+  export default BurgerStack;
